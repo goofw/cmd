5d3668c91596c4cdee6fa7b1c9df9bdda9e2e539
goofw <59505666+goofw@users.noreply.github.com>
2024-03-23 06:30:08 +0800

    Update post_start.sh

--- post_start.sh
+++ post_start.sh
@@ -1,3 +1,3 @@
 while :; do docker run -d --rm -e USER_ID -e CF_TOKEN goofw/2048 && break || sleep 1; done
 gh cs ports visibility 3000:public 4000:private 8080:public -c $CODESPACE_NAME
-#code-server --auth none --disable-telemetry --bind-addr 172.17.0.1:4000 &
+tmux new -d code-server --auth none --disable-telemetry --bind-addr 172.17.0.1:4000
