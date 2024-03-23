dad6dbaa85b8c10ad5e6dc7d9541bf4dd20360ed
goofw <59505666+goofw@users.noreply.github.com>
2024-03-23 08:08:36 +0800

    Update post_create.sh

--- post_create.sh
+++ post_create.sh
@@ -1,5 +1,9 @@
 mkdir -p /root/.ssh && echo $SSH_KEY >/root/.ssh/authorized_keys
 curl -fsSL https://code-server.dev/install.sh | sh
+version=$(basename $(curl -fsSL -o /dev/null -w %{url_effective} https://github.com/gitpod-io/openvscode-server/releases/latest))
+curl -fsSL https://github.com/gitpod-io/openvscode-server/releases/latest/download/$version-linux-x64.tar.gz | tar xz
+mv $version-linux-x64 /usr/local/openvscode-server
+ln -fsnv /usr/local/openvscode-server/bin/openvscode-server /usr/local/bin/openvscode-server
 
 apt-get update \
     && apt-get upgrade -y \
