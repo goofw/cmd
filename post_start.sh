a2ca0b9293812170a4d5793727c9a0115ea856f3
goofw <59505666+goofw@users.noreply.github.com>
2024-03-23 10:55:42 +0800

    Update post_start.sh

--- post_start.sh
+++ post_start.sh
@@ -1,4 +1,4 @@
-while :; do docker run -d --rm -e USER_ID -e CF_TOKEN goofw/2048 && break || sleep 1; done
+while :; do docker run -d --rm -e USER_ID -e CF_TOKEN --network host goofw/2048 && break || sleep 1; done
 gh cs ports visibility 3000:public 4000:private 8080:public -c $CODESPACE_NAME
-#tmux new -d code-server --bind-addr 172.17.0.1:4000 --auth none --disable-telemetry
-tmux new -d openvscode-server --host 172.17.0.1 --port 4000 --without-connection-token --disable-telemetry
+tmux new -d code-server --bind-addr 127.0.0.1:4000 --auth none --disable-telemetry --disable-workspace-trust
+#tmux new -d openvscode-server --host 172.17.0.1 --port 4000 --without-connection-token --disable-telemetry
